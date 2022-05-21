7336409519e730ea215c85da4bc0f398b568bb96
Filipe Melo <filipe.melo@liferay.com>
2022-05-21 14:27:17 -0300

    Week 1 / Day 2 - Min-Max Sum

---
+++ src/com/hackerrank/MinMaxSum/Result.java
@@ -0,0 +1,20 @@
+package com.hackerrank.MinMaxSum;
+import java.util.*;
+
+class Result {
+
+    public static void miniMaxSum(List<Integer> arr) {
+        long minSum =0, maxSum =0;
+        int auxIndex = 4;
+        Collections.sort(arr);
+        for (int i=0; i < 4; i++) {
+            minSum+=arr.get(i);
+            maxSum+=arr.get(auxIndex);
+            auxIndex--;
+        }
+
+        System.out.printf("%d %d", minSum, maxSum);
+    }
+
+}
+
