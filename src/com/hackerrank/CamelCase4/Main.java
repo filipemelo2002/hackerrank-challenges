66688a1bfd9d0c218de95c0b58ab38596804ed4c
Filipe Melo <filipe.melo@liferay.com>
2022-05-21 15:50:04 -0300

    Week 1 / Day 5 - Camel Case 4

---
+++ src/com/hackerrank/CamelCase4/Main.java
@@ -0,0 +1,68 @@
+package com.hackerrank.CamelCase4;
+
+import java.util.Locale;
+import java.util.Scanner;
+
+public class Main {
+    public static void main (String[] args) {
+        Scanner scanner=  new Scanner(System.in);
+
+        while (scanner.hasNextLine()) {
+            String line = scanner.nextLine();
+            String[] split = line.split(";");
+            String operator = split[0];
+            String type = split[1];
+            String word = split[2];
+
+            StringBuilder output = new StringBuilder();
+
+
+            if (operator.equalsIgnoreCase("s")) {
+
+                for (char c : word.toCharArray()) {
+                    if (Character.isUpperCase(c)) {
+                        output.append(" ").append(c);
+                    } else {
+                        output.append(c);
+                    }
+                }
+                output = new StringBuilder(output.toString().replace("()","").toLowerCase(Locale.ROOT));
+            }
+
+            if (operator.equalsIgnoreCase("c")) {
+                String[] words = word.split(" ");
+
+
+                for (int i =0; i < words.length; i++) {
+                    String w  = words[i];
+                    String capitalized = w.substring(0, 1).toUpperCase() + w.substring(1);
+
+                    if (type.equalsIgnoreCase("c")){
+                        output.append(capitalized);
+                    } 
+                    
+                    if (type.equalsIgnoreCase("m")) {
+                        if (i > 0) {
+                            output.append(capitalized);
+                        } else {
+                            output.append(w);
+                        }
+
+                        if (i == words.length - 1) {
+                            output.append("()");
+                        }
+                    }
+
+                    if (type.equalsIgnoreCase("v")) {
+                        if (i > 0) {
+                            output.append(capitalized);
+                        } else {
+                            output.append(w);
+                        }
+                    }
+                }
+            }
+            System.out.println(output.toString().trim());
+        }
+    }
+}
