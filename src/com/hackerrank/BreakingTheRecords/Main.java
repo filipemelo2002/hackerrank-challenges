f3de4e828e66293dd9adb1a2b80fa84fff489858
Filipe Melo <filipe.melo@liferay.com>
2022-05-21 14:47:47 -0300

    Week 1 / Day 4 - Breaking the Records

---
+++ src/com/hackerrank/BreakingTheRecords/Main.java
@@ -0,0 +1,33 @@
+package com.hackerrank.BreakingTheRecords;
+
+import java.io.*;
+import java.util.List;
+import java.util.stream.Stream;
+
+import static java.util.stream.Collectors.joining;
+import static java.util.stream.Collectors.toList;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
+
+        int n = Integer.parseInt(bufferedReader.readLine().trim());
+
+        List<Integer> scores = Stream.of(bufferedReader.readLine().replaceAll("\\s+$", "").split(" "))
+                .map(Integer::parseInt)
+                .collect(toList());
+
+        List<Integer> result = Result.breakingRecords(scores);
+
+        bufferedWriter.write(
+                result.stream()
+                        .map(Object::toString)
+                        .collect(joining(" "))
+                        + "\n"
+        );
+
+        bufferedReader.close();
+        bufferedWriter.close();
+    }
+}
---
+++ src/com/hackerrank/BreakingTheRecords/Result.java
@@ -0,0 +1,25 @@
+package com.hackerrank.BreakingTheRecords;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class Result {
+    public static List<Integer> breakingRecords(List<Integer> scores) {
+        int min=scores.get(0), max=scores.get(0), minCount=0, maxCount = 0;
+
+
+        for (int score : scores) {
+            if (score < min) {
+                min = score;
+                minCount++;
+            }
+
+            if (score > max) {
+                max = score;
+                maxCount++;
+            }
+
+        }
+        return Arrays.asList(maxCount, minCount);
+    }
+}
